bc358e5d98756d670ebf5c4fcddb6d904b0bd75e
Inletguy <kevin@cksystem.com>
2022-01-31 11:06:17 -0500

    Changes for Coursera Exam

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval+"%";
+    // Added % for readability
 }
 
 function compute()
 {
- 	var principal = parseFloat(document.getElementById("principal").value); 
-    // Converted to Floating Point for more accurate math
+    var principal = parseFloat(document.getElementById("principal").value); 
+    if (principal <=  0) {
+       alert('Enter a positive number');
+       document.getElementById("principal").focus();
+       return false;
+    }
+    // Converted to Floating Point to calculate amount
     var rate = parseFloat(document.getElementById("rate").value);
-     // Converted to Floating Point for more accurate math
+    // Converted to Floating Point to calculate amount
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var amount = principal + interest;
@@ -18,7 +24,4 @@ function compute()
     document.getElementById("interest").innerText=interest;
     document.getElementById("result").innerHTML="If you deposit $"+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of $"+amount+",\<br\>in the year "+year+"\<br\>";
 }
-function showRate(newRate)    
-{
-	document.querySelector('#Rate').value = newRate+"%"; 
-}
+
