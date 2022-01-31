f912fb30254fa0db6b8c882a7d1c3aefdf1e3352
Kevin Bowden <42843302+inletguy@users.noreply.github.com>
2022-01-31 13:24:06 -0500

    Revised Change to Simple Interest Calculator

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function compute()
        alert('Enter a positive number');
        document.getElementById("principal").focus();
        return false;
+    }  
+     
+    if (isNaN(principal)){
+       alert('Enter a positive number');
+       document.getElementById("principal").focus();
+       return false;
     }
     // Converted to Floating Point to calculate amount
     var rate = parseFloat(document.getElementById("rate").value);
@@ -20,8 +26,9 @@ function compute()
     var interest = principal * years * rate /100;
     var amount = principal + interest;
     // amount was undefined in the exercise
+//    principal.style.color="yellow";
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("interest").innerText=interest;
-    document.getElementById("result").innerHTML="If you deposit $"+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of $"+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML="If you deposit <span style='color:yellow'>$"+principal+"</span><br/>at an interest rate of <span style='color:yellow'>"+rate+"%</span></br>You will receive an amount of <span style='color:yellow'>$"+amount+"</span></br>in the year <span style='color:yellow'>"+year+"</span></br>";
 }
 
